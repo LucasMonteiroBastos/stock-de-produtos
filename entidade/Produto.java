8c520a9ee065136e880a8d3e26d1730a00a25d57
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2023-01-09 23:13:27 -0300

    Updated

--- entidade/Produto.java
+++ entidade/Produto.java
@@ -7,18 +7,22 @@ public class Produto {
 	public double preco;
 	public int quantidade;
 	
+	//metodo: funcao para calcular o valor total do estoque
 	public double totalDeValorNoEstoque( ) {
 		return preco * quantidade;
 	}
 	
+	//metodo: funcao para adicionar um produto 
 	public void adicionarProduto(int quantidade) {
 		this.quantidade += quantidade;
 	}
 	
+	//metodo: fancao para remover um produto
 	public void removerProduto(int quantidade) {
 		this.quantidade -= quantidade;
 	}
 	
+	//metodo: funcao para imprimir as informacoes
 	public String toString() {
 		return nome
 				+ ", $ "
